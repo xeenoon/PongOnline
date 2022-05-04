853a22dc055f5de14eebdb6964a8f61faa3ff9a1
Christopher <Christopher@marvica.com.au>
2022-05-04 18:06:13 +1000

    Added moveable bats

---
+++ PongOnline/Bat.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace PongOnline
+{
+    public enum MovementType
+    {
+        WASD,
+        ARROWS,
+        PORT
+    }
+    public enum Direction
+    {
+        UP,
+        DOWN,
+        NONE
+    }
+    public class Bat
+    {
+        public const int WIDTH = 10;
+        public const int HEIGHT = 30;
+
+        public static readonly Brush BRUSH = Brushes.Green;
+        Rectangle graphics;
+        Point location;
+
+        Canvas canvas;
+        MovementType movementType;
+        Direction direction = Direction.NONE;
+
+        Key up;
+        Key down;
+
+        public Bat(Canvas c, Point location, Timer mainTimer, MovementType movementType)
+        {
+            this.canvas = c;
+            graphics = new Rectangle()
+            {
+                Width = WIDTH,
+                Height = HEIGHT,
+                Fill = BRUSH,
+                Stroke = BRUSH,
+                StrokeThickness = 2,
+            };
+
+            // Add to a canvas for example
+            canvas.Children.Add(graphics);
+            Canvas.SetTop(graphics, location.Y);
+            Canvas.SetLeft(graphics, location.X);
+            this.location = location;
+
+            this.movementType = movementType;
+            switch (movementType)
+            {
+                case MovementType.WASD:
+                    up = Key.W;
+                    down = Key.S;
+                    break;
+                case MovementType.ARROWS:
+                    up = Key.Up;
+                    down = Key.Down;
+                    break;
+                case MovementType.PORT:
+                    break;
+            }
+            mainTimer.Elapsed += new ElapsedEventHandler(Tick);
+        }
+
+        public void HandleKeyDown(Key key)
+        {
+            if (key == up)
+            {
+                direction = Direction.UP;
+
+            }
+            else if (key == down)
+            {
+                direction = Direction.DOWN;
+            }
+        }
+        public void HandleKeyUp(Key key) //Consistency to have both methods accept a key
+        {
+            direction = Direction.NONE;
+        }
+        public void Tick(object sender, ElapsedEventArgs e)
+        {
+            switch (direction)
+            {
+                case Direction.UP:
+                    location.Y-=2;
+                    ModifyLocation(location.Y);
+                    break;
+                case Direction.DOWN:
+                    location.Y+=2;
+                    ModifyLocation(location.Y);
+                    break;
+                case Direction.NONE:
+                    break;
+            }
+        }
+        public void ModifyLocation(double ypos)
+        {
+            if (ypos <= 0)
+            {
+                ypos = 0;
+            }
+            if (ypos >= 435)
+            {
+                ypos = 435;
+            }
+            Application.Current.Dispatcher.Invoke((Action)(() =>
+            {
+                Canvas.SetTop(graphics, ypos);
+            }));
+        }
+    }
+}
--- PongOnline/MainWindow.xaml.cs
+++ PongOnline/MainWindow.xaml.cs
@@ -21,11 +21,42 @@ namespace PongOnline
     /// </summary>
     public partial class PongGameWindow : Window
     {
+        public static PongGameWindow Singleton;
+
         Timer t = new Timer();
+        Bat player1;
+        Bat player2;
         public PongGameWindow()
         {
             InitializeComponent();
             t.Interval = 2;
+            this.KeyDown += new KeyEventHandler(HandleKeyDown);
+            this.KeyUp   += new KeyEventHandler(HandleKeyUp);
+
+            player1 = new Bat(MainCanvas,new Point(50,50), t, MovementType.WASD);
+            player2 = new Bat(MainCanvas,new Point(630,50), t, MovementType.ARROWS);
+
+            Singleton = this;
+            t.Start();
+        }
+        public bool isKeyDown;
+        public void HandleKeyDown(object sender, KeyEventArgs e)
+        {
+            if (!isKeyDown)
+            {
+                isKeyDown = true;
+                player1.HandleKeyDown(e.Key);
+                player2.HandleKeyDown(e.Key);
+            }
+        }
+        public void HandleKeyUp(object sender, KeyEventArgs e)
+        {
+            if (isKeyDown)
+            {
+                player1.HandleKeyUp(e.Key);
+                player2.HandleKeyUp(e.Key);
+                isKeyDown = !isKeyDown;
+            }
         }
     }
 }
